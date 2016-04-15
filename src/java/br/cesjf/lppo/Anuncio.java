32a0d6cb0799606db7d6911aae3f29cd36925759
Igor Knop <igorknop@gmail.com>
2016-04-15 19:13:21 -0300

    Commit Inicial com uma classe de dominio

---
+++ src/java/br/cesjf/lppo/Anuncio.java
@@ -0,0 +1,18 @@
+package br.cesjf.lppo;
+
+public class Anuncio {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Float preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+}
